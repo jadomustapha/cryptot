3e01e77c68da3fc4a1e6f409f7fcd5845458f22d
Jadoiconic1 <iconicmustapha@gmail.com>
2022-06-03 21:53:18 +0200

    on friday  03 june

--- navbar.php
+++ navbar.php
@@ -1,6 +1,5 @@
     <nav  class=" nim-menu navbar navbar-default navbar-fixed-top">
         <div class="container">
-                
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -11,9 +10,6 @@
                 </button>
             <a class="navbar-brand" href="index.php">C<span class="themecolor">R</span>ypto Tech</a>
             </div>
-
-                    
-
                     
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -21,7 +17,7 @@
                     <li><a href="index.php" class="page-scroll"><h3>Home</h3></a></li>
                     <li><a href="product.php"><h3>Products</h3></a></li>
                     <li><a href="services.php" class="page-scroll"><h3>Services</h3></a></li>
-                    <li><a href="news.php"><h3>Hot News</h3></a></li>
+                    <!-- <li><a href="news.php"><h3>Hot News</h3></a></li> -->
                     <li><a href="signin.php"><h3>Login</h3></a></li>
                     <li><a href="#two" class="page-scroll"><h3>About</h3></a></li>
                 </ul>
